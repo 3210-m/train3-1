ea42b71172e4f2f58f5b0cde8b948383c9e8310c
tangli <985212753@qq.com>
2019-05-29 18:37:03 +0800

    practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,24 @@
 package practice05;
 
-public class Student {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student extends Person {
+    private int klass;
+
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return super.introduce()+" I am a Student. I am at Class "+this.klass+".";
+    }
 }
