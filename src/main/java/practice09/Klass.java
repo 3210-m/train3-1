65177e3d92c43fb3a4758402bac96c336506be9e
tangli <985212753@qq.com>
2019-05-29 18:40:17 +0800

    practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,46 @@
 package practice09;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+
+@Getter
+@Setter
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass().equals(this)){
+            leader = student;
+        }else {
+            System.out.printf("It is not one of us.\n");
+        }
+    }
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,33 @@
 package practice09;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
