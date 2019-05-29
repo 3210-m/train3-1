4fd38e5ac242a5ce02b871165fe8dafe8a88aac1
tangli <985212753@qq.com>
2019-05-29 18:31:11 +0800

    practice02

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,17 @@
 package practice02;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Person {
+    private String name;
+    private int age;
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,25 @@
 package practice02;
 
-public class Student {
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student extends Person{
+    private int klass;
+
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return "I am a Student. I am at Class "+this.klass+".";
+    }
 }
