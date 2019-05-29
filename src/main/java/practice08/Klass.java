9e8d8be4f5915c7f3aa26cd557322356b54ff35e
tangli <985212753@qq.com>
2019-05-29 18:39:25 +0800

    practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,23 @@
 package practice08;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student){
+        leader = student;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,29 @@
 package practice08;
 
-public class Teacher {
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        String s = super.introduce()+" I am a Teacher. I teach ";
+        return klass==null?s+"No Class.":s+klass.getDisplayName()+".";
+    }
+
+    public String introduceWith(Student student){
+        String s = "My name is Tom. I am 21 years old. I am a Teacher. I ";
+        return student.getKlass().getNumber()==(klass.getNumber())?s+"teach Jerry.":s+"don't teach Jerry.";
+    }
 }
