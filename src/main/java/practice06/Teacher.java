f0a3d4c6892e56e2a9758299f6ee0de6995b08df
tangli <985212753@qq.com>
2019-05-29 18:37:32 +0800

    practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        String s = super.introduce()+" I am a Teacher. I teach ";
+        return klass==null?s+"No Class.":s+"Class "+klass+".";
+    }
 }
