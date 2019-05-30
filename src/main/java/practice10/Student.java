d7e80fa3d2c0d9001c07ad629e0f70c5aaf4162c
tangli <985212753@qq.com>
2019-05-30 15:08:44 +0800

    practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,24 @@
 package practice10;
 
-public class Student {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        String s = super.introduce()+" I am a Student. I am ";
+        return klass.getLeader()!=null&&klass.getLeader().equals(this)?s+"Leader of Class 2.":s+"at Class 2.";
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,55 @@
 package practice10;
 
-public class Teacher {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedList;
+
+@Setter
+@Getter
+public class Teacher extends Person {
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public String introduce() {
+        String s = super.introduce() + " I am a Teacher. I teach ";
+        if (classes == null||classes.size() == 0) {
+            s = s + "No Class.";
+        } else {
+            s += "Class ";
+            for (int i = 0; i < classes.size(); i++) {
+                s = s + classes.get(i).getNumber();
+                s += i == classes.size() - 1 ? "." : ", ";
+            }
+        }
+        return s;
+    }
+
+    public String introduceWith(Student student) {
+        String s = "My name is Tom. I am 21 years old. I am a Teacher. I ";
+        s += isIn(student.getKlass().getNumber()) ? "teach Jerry." : "don't teach Jerry.";
+        //return isIn(student.getKlass().getNumber()) ? s + "teach Jerry." : s + "don't teach Jerry.";
+        return s;
+    }
+
+    public boolean isTeaching(Student student) {
+        return isIn(student.getKlass().getNumber());
+    }
+
+    public boolean isIn(int number) {
+        for (int i = 0; i < classes.size(); i++) {
+            if (classes.get(i).getNumber() == number) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
